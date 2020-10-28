d3be107575ef58ef29b03fc833734be46c71e8c7
georgina puig <georginaxcodes@gmail.com>
2020-10-28 00:58:10 -0600

    add data topline charts

--- javascripts/graph.js
+++ javascripts/graph.js
@@ -1,8 +1,12 @@
-const file = '../data/data_topline.csv';
+const dataTopline = '../data/data_topline.csv';
 
-// Parse the data and create a graph with the data.
+let salesPerMonth = [];
+let salesPerState = [];
+let salesPerSection = [];
+
+// parse the data and create a graph with the data.
 function parseData(createGraph) {
-	Papa.parse(file, {
+	Papa.parse(dataTopline, {
 		download: true,
     header: true,
 		complete: function(results) {
@@ -12,9 +16,7 @@ function parseData(createGraph) {
 	});
 }
 
-function createGraph(data) {
-  let salesPerMonth = [];
-
+function dataPer(data) {
   data.reduce((accumulator, value) => {
     if (!accumulator[value.date]) {
       accumulator[value.date] = { date: value.date, sales: 0, revenue: 0 };
@@ -25,10 +27,32 @@ function createGraph(data) {
     return accumulator;
   }, {});
 
-  // console.log(salesPerMonth);
+  data.reduce((accumulator, value) => {
+    if (!accumulator[value.estado]) {
+      accumulator[value.estado] = { estado: value.estado, sales: 0, revenue: 0 };
+      salesPerState.push(accumulator[value.estado]);
+    }
+    accumulator[value.estado].sales += parseInt(value.sales);
+    accumulator[value.estado].revenue += parseInt(value.revenue);
+    return accumulator;
+  }, {});
 
-  let chart = c3.generate({
-    bindto: '#chart',
+  data.reduce((accumulator, value) => {
+    if (!accumulator[value.canal]) {
+      accumulator[value.canal] = { canal: value.canal, sales: 0, revenue: 0 };
+      salesPerSection.push(accumulator[value.canal]);
+    }
+    accumulator[value.canal].sales += parseInt(value.sales);
+    accumulator[value.canal].revenue += parseInt(value.revenue);
+    return accumulator;
+  }, {});
+}
+
+function createGraph(data) {
+  dataPer(data);
+
+  let chart1 = c3.generate({
+    bindto: '#chart-1',
     data: {
       json: salesPerMonth,
       axes: {
@@ -51,10 +75,18 @@ function createGraph(data) {
           },
       },
       y: {
-        show: true
+        show: true,
+        label: {
+          text: 'Sales',
+          position: 'outer-middle'
+        }
       },
       y2: {
-        show: true
+        show: true,
+        label: {
+          text: 'Revenues',
+          position: 'outer-middle'
+        },
       }
     },
     size: {
@@ -62,8 +94,121 @@ function createGraph(data) {
     },
     zoom: {
         enabled: true
+    },
+    onrendered: function() {
+      d3.select( '.c3-axis-y-label' ).style( 'fill', '#1575b6' );
+      d3.select( '.c3-axis-y2-label' ).style( 'fill', '#ff7f00' );
     }
   });
+
+  let chart2 = c3.generate({
+    bindto: '#chart-2',
+    data: {
+      json: salesPerState,
+      axes: {
+        sales: 'y',
+        revenue: 'y2'
+      },
+      keys: {
+        x: 'estado',
+        value: ['sales', 'revenue']
+      }
+    },
+    axis: {
+      x: {
+          type: 'category',
+          tick: {
+            multiline: false,
+            rotate: 75,
+            
+          },
+      },
+      y: {
+        show: true,
+        label: {
+          text: 'Sales',
+          position: 'outer-middle'
+        }
+      },
+      y2: {
+        show: true,
+        label: {
+          text: 'Revenues',
+          position: 'outer-middle'
+        },
+      }
+    },
+    size: {
+      height: ( window.innerWidth < 800 ? 500 : 450 )
+    },
+    zoom: {
+        enabled: true
+    }
+  });
+
+  let chart3 = c3.generate({
+    bindto: '#chart-3',
+    data: {
+      json: salesPerSection,
+      axes: {
+        sales: 'y',
+        revenue: 'y2'
+      },
+      keys: {
+        x: 'canal',
+        value: ['sales', 'revenue']
+      },
+      type: 'bar'
+    },
+    axis: {
+      x: {
+          type: 'category',
+          tick: {
+            multiline: false,
+            culling: {
+              max: 10
+            }
+          },
+      },
+      y: {
+        show: true,
+        label: {
+          text: 'Sales',
+          position: 'outer-middle'
+        }
+      },
+      y2: {
+        show: true,
+        label: {
+          text: 'Revenues',
+          position: 'outer-middle'
+        },
+      }
+    },
+    zoom: {
+        enabled: true
+    },
+    onrendered: function() {
+      d3.select( '.c3-axis-y-label' ).style( 'fill', '#1575b6' );
+      d3.select( '.c3-axis-y2-label' ).style( 'fill', '#ff7f00' );
+    }
+  });
+
+  setTimeout(function () {
+    chart3.transform('bar', 'sales');
+  }, 1000);
+
+  setTimeout(function () {
+    chart3.transform('bar', 'revenue');
+  }, 2000);
+
+  setTimeout(function () {
+    chart3.transform('line');
+  }, 3000);
+
+  setTimeout(function () {
+    chart3.transform('bar');
+  }, 4000);
 }
 
 parseData(createGraph);
