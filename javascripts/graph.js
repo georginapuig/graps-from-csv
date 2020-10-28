d4ea126e1161725fabb5ac6c45677ed2106a14a1
georgina puig <georginaxcodes@gmail.com>
2020-10-27 19:45:19 -0600

    sales and revenue per month graph

--- javascripts/graph.js
+++ javascripts/graph.js
@@ -4,25 +4,64 @@ const file = '../data/data_topline.csv';
 function parseData(createGraph) {
 	Papa.parse(file, {
 		download: true,
+    header: true,
 		complete: function(results) {
-      console.log(results.data);
+      // console.log(results.data);
 			createGraph(results.data);
-		}
+    }
 	});
 }
 
 function createGraph(data) {
-  var chart = c3.generate({
+  let salesPerMonth = [];
+
+  data.reduce((accumulator, value) => {
+    if (!accumulator[value.date]) {
+      accumulator[value.date] = { date: value.date, sales: 0, revenue: 0 };
+      salesPerMonth.push(accumulator[value.date]);
+    }
+    accumulator[value.date].sales += parseInt(value.sales);
+    accumulator[value.date].revenue += parseInt(value.revenue);
+    return accumulator;
+  }, {});
+
+  // console.log(salesPerMonth);
+
+  let chart = c3.generate({
+    bindto: '#chart',
     data: {
-        columns: [
-            ['data1', 30, 200, 100, 400, 150, 250, 50, 100, 250]
-        ]
+      json: salesPerMonth,
+      axes: {
+        sales: 'y',
+        revenue: 'y2'
+      },
+      keys: {
+        x: 'date',
+        value: ['sales', 'revenue']
+      }  
     },
     axis: {
-        x: {
-            type: 'category',
-            categories: ['cat1', 'cat2', 'cat3', 'cat4', 'cat5', 'cat6', 'cat7', 'cat8', 'cat9']
-        }
+      x: {
+          type: 'category',
+          tick: {
+            multiline: false,
+            culling: {
+              max: 10
+            }
+          },
+      },
+      y: {
+        show: true
+      },
+      y2: {
+        show: true
+      }
+    },
+    size: {
+      height: ( window.innerWidth < 800 ? 500 : 300 )
+    },
+    zoom: {
+        enabled: true
     }
   });
 }
